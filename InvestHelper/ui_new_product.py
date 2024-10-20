e519f1371e6e814de6a3c9b7307e4edc8ed92db5
Mingqian <mq_han@hotmail.com>
2024-10-20 10:27:40 +0800

    Refactor new product window.

--- InvestHelper/ui_new_product.py
+++ InvestHelper/ui_new_product.py
@@ -4,55 +4,65 @@ from investment import Investment
 from common_util import compare_date_str, event_queue
 
 
-def show_new_product_window():
-    investment = Investment('./money.db')
+# Constants
+VALID_INPUT_CHARS = '0123456789.'
 
-    layout = [
-        [sg.Text(lang['Code']), sg.InputText(key='Code')],
-        [sg.Text(lang['Name']), sg.InputText(key='Name')],
-        [sg.Text(lang['Date']), sg.Input(key='Date'),
-         sg.CalendarButton('...', target='Date', format='%Y-%m-%d')],
-        [sg.Text(lang['Baseline']), sg.InputText(key='Baseline')],
-        [sg.Text(lang['StocksPct']), sg.InputText(key='StocksPct')],
-        [sg.Text(lang['BondsPct']), sg.InputText(key='BondsPct')],
-        [sg.Text(lang['Rating']),
-         sg.Combo(['R1', 'R2', 'R3', 'R4', 'R5'], default_value='R2', key='Rating', )],
-        [sg.Text(lang['Capital']), sg.InputText(key='Capital', enable_events=True)],
-        [sg.Text(lang['Share']), sg.InputText(key='Share', enable_events=True)],
-        [sg.Text(lang['Price']), sg.InputText(key='Price', enable_events=True)],
-        [sg.Text(lang['Due']), sg.InputText(key='Due'), sg.CalendarButton('...', target='Due', format='%Y-%m-%d')],
-        [sg.Button(lang['Ok'], key='Ok'), sg.Button(lang['Cancel'], key='Cancel')]
-    ]
 
-    window = sg.Window(lang['Add'], layout)
-    result = None
+class NewProductWindow:
+    def __init__(self):
+        self.investment = Investment('money.db')
+        self.window = self.create_window()
 
-    while True:
-        event, values = window.read()
+    @staticmethod
+    def create_window():
+        layout = [
+            [sg.Text(lang['Code']), sg.InputText(key='Code')],
+            [sg.Text(lang['Name']), sg.InputText(key='Name')],
+            [sg.Text(lang['Date']), sg.Input(key='Date'), sg.CalendarButton('...', target='Date', format='%Y-%m-%d')],
+            [sg.Text(lang['Baseline']), sg.InputText(key='Baseline')],
+            [sg.Text(lang['StocksPct']), sg.InputText(key='StocksPct')],
+            [sg.Text(lang['BondsPct']), sg.InputText(key='BondsPct')],
+            [sg.Text(lang['Rating']), sg.Combo(['R1', 'R2', 'R3', 'R4', 'R5'], default_value='R2', key='Rating')],
+            [sg.Text(lang['Capital']), sg.InputText(key='Capital', enable_events=True)],
+            [sg.Text(lang['Share']), sg.InputText(key='Share', enable_events=True)],
+            [sg.Text(lang['Price']), sg.InputText(key='Price', enable_events=True)],
+            [sg.Text(lang['Due']), sg.InputText(key='Due'), sg.CalendarButton('...', target='Due', format='%Y-%m-%d')],
+            [sg.Button(lang['Ok'], key='Ok'), sg.Button(lang['Cancel'], key='Cancel')]
+        ]
+        return sg.Window(lang['Add'], layout)
 
+    def validate_input(self, event, values):
         if event in ('Capital', 'Share', 'Price'):
-            if len(values[event]) and values[event][-1] not in '0123456789.':
-                window[event].update(values[event][:-1])
+            if len(values[event]) and values[event][-1] not in VALID_INPUT_CHARS:
+                self.window[event].update(values[event][:-1])
+
+    def handle_event(self, event, values):
+        if event in (sg.WIN_CLOSED, 'Cancel'):
+            return False
 
         if event == 'Ok':
-            products = investment.fetch_products()
-            existed = False
-            if products is not None:
-                for p in products:
-                    if values['Code'] == p['id']:
-                        sg.popup(lang['Existed'])
-                        existed = True
-                        break
-            if not existed:
-                if compare_date_str(values['Date'], values['Due']):
-                    sg.popup(lang['Data Wrong'])
-                else:
-                    investment.add_product(values)
-                    result = 'Refresh'
-                    break
+            products = self.investment.fetch_products()
+            if products and any(p['id'] == values['Code'] for p in products):
+                sg.popup(lang['Existed'])
+            elif compare_date_str(values['Date'], values['Due']):
+                sg.popup(lang['Data Wrong'])
+            else:
+                self.investment.add_product(values)
+                event_queue.put('Refresh')
+                return False
 
-        if event in (sg.WIN_CLOSED, 'Cancel'):  # always check for closed window
-            break
+        return True
 
-    event_queue.put('Refresh')
-    window.close()
+    def run(self):
+        while True:
+            event, values = self.window.read()
+            self.validate_input(event, values)
+            if not self.handle_event(event, values):
+                break
+
+        self.window.close()
+
+
+def show_new_product_window():
+    new_window = NewProductWindow()
+    new_window.run()
